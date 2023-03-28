4775ac086033beae125792ca7c2fd375a85a92fa
Rebecca Kelsall <rebecca.kelsall@sky.uk>
2023-03-28 15:44:39 +0100

    Added order script

---
+++ Empower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D8D0CF6D-1D91-4A2E-BD38-434CD1BC6BAA")]
+[assembly: AssemblyVersion("1.0.0.0")]
