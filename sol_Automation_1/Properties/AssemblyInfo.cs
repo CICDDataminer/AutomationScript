f100cab5225acbf740fb93d81471f7edf5b4ffc9
RKS02 <RKS02@UK6DLK7M3>
2023-03-28 13:17:54 +0100

    Add project files.

---
+++ sol_Automation_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("sol_Automation_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("sol_Automation_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("BF7ECA81-17D2-41B5-AB2F-E95C0801CDF7")]
+[assembly: AssemblyVersion("1.0.0.0")]
